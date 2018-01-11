52d3a27b02f64155cd36d2b759923a8b25d438b3
jtkuhn3 <jtkuhn3@ncsu.edu>
2018-01-11 14:23:19 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,14 +4,21 @@
 //01/06/18
 //for now, I'm just going to setup a welcome message and 10 buttons for the index for the art
 var button = [];
+var frm;
+var enteredNum;
 
 function setup() {
   createCanvas(300,300);
+  frm = createElement('form','this sends to the back-end');
+  
   background(255);
     
   for(i=0;i<10;i++){
        button[i] = createButton(i);
        button[i].mousePressed(changeBG);
+       button[i].style("color:#427E93; border-color:#000000; background-color:#4156A1;");
+                       //using ncsu branding colors
+       
   }
  
   button[1].position(50,50);
@@ -35,9 +42,19 @@ function draw() {
   fill(0);    
   textFont('Helvetica',12);
   text("Click on the numpad \nto change the background", 25,25);
+  text(enteredNum, 30,30);
 }
 
 function changeBG() {
   var val = random(255);
   background(val,20,200);
 }
+
+/* Checklist
+
+1.  Styling
+2.  Input
+3.  HTML Form
+4.  Color
+5.  Animation
+ */
